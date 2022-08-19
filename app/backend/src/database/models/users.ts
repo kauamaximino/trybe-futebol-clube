3435489b0b5f4c839b77552845c94ff489f6ff73
Kauã Maximino <kaua_maximino@outlook.com>
2022-08-19 15:50:39 -0300

    feat: testando req 01

---
+++ app/backend/src/database/models/users.ts
@@ -0,0 +1,41 @@
+import { Model, STRING, INTEGER } from 'sequelize';
+import db from '.';
+
+class Users extends Model {
+  id!: number;
+  username!: string;
+  role!: string;
+  email!: string;
+  password!: string;
+}
+
+Users.init({
+  id: {
+    type: INTEGER,
+    primaryKey: true,
+    autoIncrement: true,
+    allowNull: false,
+  },
+  username: {
+    type: STRING,
+    allowNull: false,
+  },
+  role: {
+    type: STRING,
+    allowNull: false,
+  },
+  email: {
+    type: STRING,
+    allowNull: false,
+  },
+  password: {
+    type: STRING,
+    allowNull: false,
+  },
+}, {
+  sequelize: db,
+  modelName: 'users',
+  underscored: true,
+});
+
+export default Users;
