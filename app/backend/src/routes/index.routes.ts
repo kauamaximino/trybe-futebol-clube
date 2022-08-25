aecfc6d75c77c3dc047bdf1c4afcab3baec2a1e2
Kauã Maximino <kaua_maximino@outlook.com>
2022-08-25 15:41:22 -0300

    :sparkles:	feat: importando rota teams para o index e utilizando no "use"

--- app/backend/src/routes/index.routes.ts
+++ app/backend/src/routes/index.routes.ts
@@ -1,8 +1,10 @@
 import * as express from 'express';
 import login from './login.routes';
+import teams from './teams.routes';
 
 const router = express.Router();
 
 router.use(login);
+router.use(teams);
 
 export default router;
