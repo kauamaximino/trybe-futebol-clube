ad0f06f2bd62979fbcfce0846574510bd4484190
Kauã Maximino <kaua_maximino@outlook.com>
2022-08-25 15:39:56 -0300

    :test_tube:	test: implementando teste da rota "/login/validate"

--- app/backend/src/tests/usersAndLogin.test.ts
+++ app/backend/src/tests/usersAndLogin.test.ts
@@ -28,16 +28,26 @@ describe('Seção 1: Users e Login', () => {
     password: '$2a$08$xi.Hxk1czAO0nZR..B393u10aED0RQ1N3PAEXQ7HxtLjKPEZBu.PW',
   }
 
-    it('retorna status 200 e o token JWT', async () => {
-      sinon.stub(Users, 'findOne').resolves(mockDB as Users)
+  const mockToken = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJjcmVkZW50aWFscyI6eyJlbWFpbCI6ImFkbWluQGFkbWluLmNvbSIsInBhc3N3b3JkIjoic2VjcmV0X2FkbWluIiwicm9sZSI6ImFkbWluIn0sImlhdCI6MTY2MTQzODUyMCwiZXhwIjoxNjY0MDMwNTIwfQ.XsnXjsfUnqSqntsrbPjb4Bf8OmwaUPYmi8xTHH9hD80';
 
-      chaiHttpResponse = await chai.request(app)
-      .post('/login')
-      .send(mockBody)
-      
-      expect(chaiHttpResponse.status).to.equal(200)
-      expect(chaiHttpResponse.body).to.have.property('token')
+  it('Deve retornar status 200 e o token JWT', async () => {
+    sinon.stub(Users, 'findOne').resolves(mockDB as Users)
 
-      sinon.restore()
+    chaiHttpResponse = await chai.request(app)
+    .post('/login')
+    .send(mockBody)
+    
+    expect(chaiHttpResponse.status).to.equal(200)
+    expect(chaiHttpResponse.body).to.have.property('token')
+
+    sinon.restore()
+  });
+
+  it('Deve validar corretamente a rota "/login/validate"', async () => { 
+    chaiHttpResponse = await chai.request(app)
+    .get('/login/validate')
+    .send(mockToken)
+
+    expect(chaiHttpResponse.status).to.equal(200)
   });
 })
